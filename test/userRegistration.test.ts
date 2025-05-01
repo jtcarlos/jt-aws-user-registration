8e77dff4cf9f3f5912c0a0367015c7f9589c72b6
Thom Carlos <john.thomas.carlos0913@gmail.com>
2025-05-01 09:31:42 +0800

    app: initial commit

---
+++ test/userRegistration.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JtAwsUserRegistration from '../lib/jt-aws-user-registration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jt-aws-user-registration-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new JtAwsUserRegistration.JtAwsUserRegistrationStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+})
